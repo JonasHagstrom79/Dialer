2033eee479aecb03c75be66c14fbd4e7fe708785
Jonas Hagström <caha1906@student.miun.se>
2022-11-04 10:33:20 +0100

    new activity for dial buton

---
+++ app/src/main/java/se/miun/caha1906/dt031g/dialer/DialActivity.java
@@ -0,0 +1,14 @@
+package se.miun.caha1906.dt031g.dialer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dial);
+    }
+}
