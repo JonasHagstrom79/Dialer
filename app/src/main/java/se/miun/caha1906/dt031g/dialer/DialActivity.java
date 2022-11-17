9bc9cf5066debee2b1cc82cb889c27fd1def9c50
Jonas Hagström <caha1906@student.miun.se>
2022-11-17 15:16:14 +0100

    added attr.xml view_dialpadbutton scales correct removed comments

--- app/src/main/java/se/miun/caha1906/dt031g/dialer/DialActivity.java
+++ app/src/main/java/se/miun/caha1906/dt031g/dialer/DialActivity.java
@@ -11,12 +11,6 @@ public class DialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial);
 
-
-        //DialpadButton myButton = new DialpadButton(this); // Gör så skiten hänger sig
-        //myButton.setTitle("3"); // Fungerar ej!
-        //myButton.setMessage("ABCF"); // Fungerar ej!
-        //setContentView(R.layout.dialpadbutton); // Fungerar!! :)
-        //setContenView(R.layout.dialpadbutton);
     }
 
 
--- app/src/main/java/se/miun/caha1906/dt031g/dialer/DialpadButton.java
+++ app/src/main/java/se/miun/caha1906/dt031g/dialer/DialpadButton.java
@@ -2,34 +2,101 @@ package se.miun.caha1906.dt031g.dialer;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class DialpadButton extends ConstraintLayout {
 
+
+    private static final String UNDEFINED = "";
+
+    /**
+     * Default constructor for java code
+     */
     public DialpadButton(@NonNull Context context) {
         super(context);
-        init(context);
+        init(null);
     }
 
+    /**
+     * Costum constructor to use when setting title and message at the same time
+     *  we create an instance.
+     */
+    public DialpadButton(@NonNull Context context, String title, String message) {
+        super(context);
+        init(null);
+        setTitle(title);
+        setMessage(message);
+    }
+
+    /**
+     * Constructor used when creating instances of an xml file
+     */
     public DialpadButton(@NonNull Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context);
+        init(attrs);
     }
 
-    @SuppressLint("ClickableViewAccessibility")
-    private void init(Context context) {
-        //inflate(context, R.layout.dialpadbutton, this);
-        inflate(context, R.layout.view_dialpadbutton, this);
+    /**
+     * Takes care of all initialization the component needs     *
+     */
+    private void init(AttributeSet attrs) {
+        inflate(getContext(), R.layout.view_dialpadbutton, this);
 
+        // Animates the view
+        animateClick();
+
+        // Retrieve custom attribues
+        TypedArray customAttributes = getContext().getTheme().obtainStyledAttributes(
+                attrs, // The base set of attribute values. May be null.
+                R.styleable.DialpadButton, // Our custom attributes to be retrieved (in res/values/attrs.xml).
+                0,0); // 0 = do to not look for default values
+
+
+        String mess;
+        String title;
+
+        // Get the values for the custom attributes
+        try {
+            mess = customAttributes.getString(
+                    R.styleable.DialpadButton_message);
+            title = customAttributes.getString(
+                    R.styleable.DialpadButton_title);
+        } finally {
+            customAttributes.recycle();
+        }
+
+        // Update the view to show string from attribute
+        setMessage(mess);
+        setTitle(title);
+
+        // Call setSaveEnabled(true) to indicate that this view intends to save its state.
+        setSaveEnabled(true);
+
+    }
+
+
+    /**
+     * Animates the click on the component
+     */
+    @SuppressLint("ClickableViewAccessibility")
+    private void animateClick() {
         setOnTouchListener((view, motionEvent) -> {
+
             switch (motionEvent.getAction()) {
+                // When pushed
                 case MotionEvent.ACTION_DOWN:
                     animate().alpha(0f).setDuration(500).start();
                     break;
+                // When released
                 case MotionEvent.ACTION_UP:
                     animate().alpha(1f).setDuration(500).start();
                     break;
@@ -38,66 +105,46 @@ public class DialpadButton extends ConstraintLayout {
         });
     }
 
-//    // Does not make use of XML attributes
-//    public DialpadButton(Context context) {
-//        super(context);
-//
-//        // Add init method?
-////        init(null);
-//    }
-//
-//
-//    //This constructor is used when you create instances from an xml layout file.
-//    //For example, from activity_main.xml (where app:color is a custom attribute we
-//    //can use to set starting color for the button):
-//    public DialpadButton(Context context, AttributeSet attrs) {
-//        super(context, attrs);
-//
-//        //return attrs;
-//    }
+    /**
+     *  Sets the title of the view
+     * @param newTitle string to be set
+     */
+    public void setTitle(@NonNull String newTitle) {
 
+        if (TextUtils.isEmpty(newTitle)) {
 
-    // 3 more ctors in original code?
+            return;
 
-    //ObjectAnimator alphaAnimator = ObjectAnimator.ofFloat()
+        } else {
+            String t = newTitle.substring(0,1).toUpperCase();
+            TextView title = findViewById(R.id.textTitle);
+            title.setText(t);
+        }
 
-
-
-
-    // Use a private method to do the init
-//    private void init(AttributeSet attrs) {
-//        // Inflate the layout this component uses
-//        inflate(getContext(), R.layout.dialpadbutton, this);
-//
-//        // Now we can get a reference to the views this component uses
-//        dialPadMessage = findViewById(R.id.textDialpadMessage);
-//        dialPadTitle = findViewById(R.id.textDialpadTitle);
-//
-//        ValueAnimator animation = ValueAnimator.ofFloat(0f, 100f); // TEST
-//        animation.setDuration(1000); // Test
-//        animation.start(); // Test
-//        // Retrieve our custom attributes.
-//        //TypedArray customAttributes = getContext().getTheme().obtainStyledAttributes(
-//        //        attrs,
-//        //        R.styleable.DialpadButton, // HÄR ÄR JAG!!!!
-//        //        0, 0);
-//
-//
-//    }
-        //inflate(getContext(), R.layout.dialpadbutton, this);
-
-    // Det ska finnas en metod som kan användas för att ändra vad som ska
-    //TextView myTitle = findViewById(R.id.textDialpadTitle);
-    // visas som title
-    public void setTitle(String newTitle) {
-        //myTitle.setText(newTitle);
-        //return title;
     }
 
-    //TextView myMessage = findViewById(R.id.textDialpadMessage);
-    // Visar message under title
-    public void setMessage(String newMessage) {
-        // Sets message
-        //myMessage.setText(newMessage);
+    /**
+     * Sets the message for the view
+     * @param newMessage string to be set
+     */
+    public void setMessage(@NonNull String newMessage) {
+
+        String mess;
+
+        if (TextUtils.isEmpty(newMessage)) {
+
+            return;
+
+        } else {
+
+            if (newMessage.length() > 4) {
+                mess = newMessage.substring(0, 4).toUpperCase();
+            } else {
+                mess = newMessage.toUpperCase();
+            }
+        }
+
+        TextView message = findViewById(R.id.textMessage);
+        message.setText(mess);
     }
 }
