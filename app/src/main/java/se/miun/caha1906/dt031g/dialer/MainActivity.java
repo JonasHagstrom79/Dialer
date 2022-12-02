02207184c91bf3300f36459a3b734174a1ac4f8f
Jonas Hagström <caha1906@student.miun.se>
2022-12-02 15:12:38 +0100

    util.java added

--- app/src/main/java/se/miun/caha1906/dt031g/dialer/MainActivity.java
+++ app/src/main/java/se/miun/caha1906/dt031g/dialer/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     SoundPlayer s;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
