021807a27e65ba521455a20b3f7aae0acd08b369
Jonas Hagström <caha1906@student.miun.se>
2022-12-02 17:25:35 +0100

    writetofile seems to work

--- app/src/main/java/se/miun/caha1906/dt031g/dialer/MainActivity.java
+++ app/src/main/java/se/miun/caha1906/dt031g/dialer/MainActivity.java
@@ -4,14 +4,18 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SharedElementCallback;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileWriter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +24,25 @@ public class MainActivity extends AppCompatActivity {
 
     SoundPlayer s;
 
-
+    String defaultVoice = Util.DEFAULT_VOICE; //Nytt
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //File from = new File() //Fungerqr ej
+        File newFile = new File(this.getFilesDir(), "text.txt");
+        File u = Util.getDirForDefaultVoice(this); ///data/user/0/se.miun.caha1906.dt031g.dialer/files/voices/mamacita_us
+        File x = Util.getInternalStorageDir(this); ///data/user/0/se.miun.caha1906.dt031g.dialer/files
+        this.deleteFile("NewTextFile.txt");
+        //TESTAR, alla filer kom in när jag använde den här metoden??
+        writeFileOnInternalStorage(this,"rolig.txt","Snart är det jul");
+        File file = new File(u, "text.txt"); ///data/user/0/se.miun.caha1906.dt031g.dialer/files/text.txt
+
+        Util.copyDefaultVoiceToInternalStorage(this);
+
+
 
         Log.d("Mainactivity","L25 onCreate"); //TODO: Remove!
 
@@ -98,6 +115,81 @@ public class MainActivity extends AppCompatActivity {
         outState.putBoolean("Aboutvalue", about);
 
         super.onSaveInstanceState(outState);
+    }
+
+    @Override //När man fortsätter kanske
+    protected  void onResume() {
+        super.onResume(); // Always call super first
+
+        storeSound();
+    }
+
+    @Override //Save data in onPause
+    protected void onPause() {
+        super.onPause();
+        // Från youtube2 4:27
+//        SharedPreferences.Editor preferencesEditor =
+//                mPreferences.edit();
+
+
+        //Spara ljuden?
+        writeFileOnInternalStorage(this,"rolig.txt","Snart är det jul");
+    }
+
+    public void writeFileOnInternalStorage(Context mcoContext, String sFileName, String sBody){
+            File dir = new File(mcoContext.getFilesDir(), "mydir");
+            if(!dir.exists()){
+                dir.mkdir();
+            }
+
+            try {
+                File gpxfile = new File(dir, sFileName);
+                FileWriter writer = new FileWriter(gpxfile);
+                writer.append(sBody);
+                writer.flush();
+                writer.close();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+    }
+
+    private void storeSound() {
+
+
+
+
+        File localDir = getApplicationContext().getFilesDir(); //localDir: "/data/user/0/se.miun,caha1906.dt031g.dialer.files"
+
+//        String test = "afag";
+//        File file = new File(
+//                getApplicationContext(), "filename", "clicks.txt");
+
+//        try (PrintWriter out = new PrintWriter(new FileWriter(
+//                new File(localDir, Util..getFilename(this))))) {
+//            out.println(button1Clicks);
+//            out.println(button2Clicks);
+//        } catch (IOException e) {
+//            // If an IOException occurs, we just ignore it
+//            Log.e(TAG, "Error writing number of clicks to file: " + e.getMessage());
+//        }
+        // Från stackoverflow nedan
+//        public void writeFileOnInternalStorage(Context mcoContext, String sFileName, String sBody){
+//            File dir = new File(mcoContext.getFilesDir(), "mydir");
+//            if(!dir.exists()){
+//                dir.mkdir();
+//            }
+//
+//            try {
+//                File gpxfile = new File(dir, sFileName);
+//                FileWriter writer = new FileWriter(gpxfile);
+//                writer.append(sBody);
+//                writer.flush();
+//                writer.close();
+//            } catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+
     }
 
 
