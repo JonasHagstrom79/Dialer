4524333603f53fb563210b6597a41d1182f4884f
Jonas Hagström <caha1906@student.miun.se>
2023-02-23 17:35:43 +0100

    Task 5 done

--- app/src/main/java/se/miun/caha1906/dt031g/dialer/DownloadActivity.java
+++ app/src/main/java/se/miun/caha1906/dt031g/dialer/DownloadActivity.java
@@ -58,6 +58,7 @@ public class DownloadActivity extends AppCompatActivity {
         //Get the views
         findViews();
 
+        // Initialize the progress dialog
         progressDialog = new ProgressDialog(this);
         progressDialog.setMessage("Downloading...");
         progressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
@@ -76,7 +77,6 @@ public class DownloadActivity extends AppCompatActivity {
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
 
                 // Load all URL links inside the WebView
-
                 String url = request.getUrl().toString();
 
                 view.loadUrl(url);
@@ -93,10 +93,10 @@ public class DownloadActivity extends AppCompatActivity {
             public void onDownloadStart(String url, String userAgent, String contentDisposition,
                                         String mimetype, long contentLength) {
 
-
                 // Show the progress dialog
                 progressDialog.show();
 
+                // Add a delay of 20ms
                 new Handler().postDelayed(new Runnable() {
                     @Override
                     public void run() {
@@ -180,156 +180,12 @@ public class DownloadActivity extends AppCompatActivity {
                 });
             }
 
-//                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));//TODO:laddar ner till downloads
-//                request.setMimeType(mimetype);
-//                // Ange sökvägen för att spara nedladdade filen
-//                String fileName = URLUtil.guessFileName(url, contentDisposition, mimetype);
-//                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
-//                //request.setD
-//                DownloadManager downloadManager = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-//                // Starta nedladdning och visa en förloppsindikator
-//                downloadManager.enqueue(request);//TODO:laddar ner till downloads
-
-//                String filename = "filename.mp3";
-//                File file = new File(getFilesDir(), "voices/" + filename);
-//
-//                try (InputStream in = new URL(url).openStream();
-//                     OutputStream out = new FileOutputStream(file)) {
-//                    // Kopiera data från in till out med hjälp av en byte buffer
-//                    byte[] buffer = new byte[2048];
-//
-//                    int bytesRead;
-//                    while ((bytesRead = in.read(buffer)) != -1) {
-//                        out.write(buffer, 0, bytesRead);
-//                    }
-//                } catch (IOException e) {
-//                    Log.e(TAG, "Error copying file: " + e.getMessage());
-//                    //return false;
-//                }
-//
-//
-//                //return false;
-//            }
         };
 
         // Set the custom DownloadListener to the WebView
         webView.setDownloadListener(downloadListener);
 
-        // Load the URL into the WebView
-        //webView.loadUrl("https://example.com");
     }
-
-
-
-
-
-
-    private class DownloadTask extends AsyncTask<String, Void, Void> { //TODO:för download
-
-        @Override
-        protected Void doInBackground(String... params) {
-            String url = params[0];
-            String voiceStoragePath = params[1];
-
-
-
-            // Skapa en fil för nedladdningen
-            File file = new File(getExternalFilesDir(null), "download.zip");
-
-            try {
-                // Ladda ner filen från URL
-                URLConnection connection = new URL(url).openConnection();
-                InputStream input = new BufferedInputStream(connection.getInputStream());
-                OutputStream output = new FileOutputStream(file);
-
-                byte[] data = new byte[1024];
-                int count;
-                while ((count = input.read(data)) != -1) {
-                    output.write(data, 0, count);
-                }
-
-                output.flush();
-                output.close();
-                input.close();
-
-                // Packa upp filen
-                ZipFile zipFile = new ZipFile(file);
-                Enumeration<? extends ZipEntry> entries = zipFile.entries();
-                while (entries.hasMoreElements()) {
-                    ZipEntry entry = entries.nextElement();
-                    File entryFile = new File(getExternalFilesDir(null), voiceStoragePath + entry.getName());
-                    if (entry.isDirectory()) {
-                        entryFile.mkdirs();
-                    } else {
-                        InputStream zipInput = zipFile.getInputStream(entry);
-                        OutputStream zipOutput = new FileOutputStream(entryFile);
-
-                        byte[] zipData = new byte[1024];
-                        int zipCount;
-                        while ((zipCount = zipInput.read(zipData)) != -1) {
-                            zipOutput.write(zipData, 0, zipCount);
-                        }
-
-                        zipOutput.flush();
-                        zipOutput.close();
-                        zipInput.close();
-                    }
-                }
-
-                // Radera den nedladdade filen
-                file.delete();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-    }
-
-    // Använd DownloadTask för att starta nedladdning och uppackning i bakgrunden
-    //DownloadTask downloadTask = new DownloadTask();
-    //downloadTask.execute(url, voiceStoragePath);
-
-
-//    private ProgressDialog mProgressDialog;
-//
-//    // Metod för att ladda ner filen från URL
-//    private void downloadFile(String fileUrl, String fileName) {
-//        try {
-//            URL url = new URL(fileUrl);
-//            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-//            urlConnection.setRequestMethod("GET");
-//            urlConnection.setDoOutput(true);
-//            urlConnection.connect();
-//
-//            // Ladda ner filen till enheten
-//            File dir = new File(Environment.getExternalStorageDirectory().getPath() + "/voices");
-//            if (!dir.exists()) {
-//                dir.mkdirs();
-//            }
-//            File file = new File(dir, fileName);
-//            FileOutputStream fileOutput = new FileOutputStream(file);
-//            InputStream inputStream = urlConnection.getInputStream();
-//
-//            byte[] buffer = new byte[1024];
-//            int bufferLength = 0;
-//
-//            while ((bufferLength = inputStream.read(buffer)) > 0) {
-//                fileOutput.write(buffer, 0, bufferLength);
-//            }
-//
-//            fileOutput.close();
-//            urlConnection.disconnect();
-//
-//            // Packa upp filen och radera zipfilen
-//            //Util.unzip(file.getPath(), dir.getPath());
-//            file.delete();
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
 
     /**
      * Get the views
